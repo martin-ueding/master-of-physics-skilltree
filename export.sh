d7f03e17b7d511f897798215f2c86fca86046947
Martin Ueding <dev@martin-ueding.de>
2014-02-25 17:39:01 +0100

    Clean up

--- export.sh
+++ export.sh
@@ -7,3 +7,6 @@ set -u
 pdfcrop Mastermodule.pdf crop.pdf
 pdftk A=crop.pdf cat Aeast output rotated.pdf
 pdftoppm -singlefile -scale-to 8000 -png rotated.pdf raster
+
+rm -f crop.pdf
+rm -f rotated.pdf
